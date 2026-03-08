7e57d5e6728a79851284fae089c1ce5cf53eb540
Liz-nor <lise-ervik@hotmail.com>
2026-03-08 14:20:04 +0100

    Testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,24 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+/** @type {import('eslint').Linter.Config[]} */
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        describe: true,
+        test: true,
+        it: true,
+        expect: true,
+        require: true,
+        module: true,
+        process: true,
+      },
+    },
+  },
 ]);
